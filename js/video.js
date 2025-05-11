5cd392cc785e42988c6f403877c5023e2f55d87b
Rashikchowdhury <muhammadrashikchowdhury@gmail.com>
2025-05-11 09:31:24 +0600

    fetched categories

--- js/video.js
+++ js/video.js
@@ -1,1 +1,22 @@
-console.log("file connected successfully");
+const fetchCategories = async () => {
+    try {
+        let res = await fetch('https://openapi.programming-hero.com/api/phero-tube/categories');
+        let data = await res.json();
+        loadCategories(data.categories);
+    }
+    catch(error) {
+        console.error(`Error happend: ${error}`)
+    }
+}
+
+const loadCategories = (categories) => {
+    let categoriesContainer = document.getElementById("categories-container");
+    
+    categories.forEach(data => {
+        console.log(data.category);
+    })
+    
+}
+
+fetchCategories()
+
